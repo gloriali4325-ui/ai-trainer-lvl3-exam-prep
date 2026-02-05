5a6758e2dbc2d6288e6c5ab4566c8bd3f86961eb
Li Gloria <242357943+gloriali4325-ui@users.noreply.github.com>
2026-02-05 18:03:05 +0800

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a1abbd753064e0a667059ce961dc83a6",
+const RESOURCES = {"flutter_bootstrap.js": "fec2c256737e73fdc7d71b1fc1c397cc",
 "version.json": "78889804c23d5fe7d08e15381ac1e098",
 "index.html": "448ac7f86035cb0471bda23e968d653e",
 "/": "448ac7f86035cb0471bda23e968d653e",
